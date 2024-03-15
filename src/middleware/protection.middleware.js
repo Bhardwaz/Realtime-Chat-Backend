1e0b4dde9ed2e4f890b523d5c0a1f2ec6bcdd290
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-03-15 15:26:45 +0530

    req.cookies removed for cross site cookies error - req.headers added

--- src/middleware/protection.middleware.js
+++ src/middleware/protection.middleware.js
@@ -1,16 +1,28 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 const protection = async (req, res, next) => {
-  try {
-    const token = req.cookies.accessToken;
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    req.user = await User.findById(decoded.id).select("-password");
-    next();
-  } catch (error) {
-    console.log("token authorization failed", error);
-    throw new Error("Not authorized, token failed");
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log("token authorization failed", error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
 };
 
